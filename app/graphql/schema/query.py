0b77e4373509c32f72c6b3a336dd7ee1bc5e9f70
Christian Hettlage <hettlage@saao.ac.za>
2018-10-23 15:27:58 +0200

    removed whoAmI query

--- app/graphql/schema/query.py
+++ app/graphql/schema/query.py
@@ -1,6 +1,5 @@
 from graphene import Field, NonNull, ObjectType, String
 from app.graphql.schema.auth_token import AuthToken
-from app.graphql.schema.who_am_i import WhoAmI
 
 
 class Query(ObjectType):
@@ -9,11 +8,5 @@ class Query(ObjectType):
                        username=NonNull(String, description='username'),
                        password=NonNull(String, description='Password'))
 
-    who_am_i = Field(lambda: WhoAmI,
-                     description='A description of the user making the query.')
-
     def resolve_auth_token(self, info, username, password):
         return dict(username=username, password=password)
-
-    def resolve_who_am_i(self, info):
-        return {}
